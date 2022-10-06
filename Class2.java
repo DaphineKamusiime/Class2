a8a368099c9b8c7c3d3ccb859dc9334c5e30fb72
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2022-10-06 06:55:22 -0700

    Add files via upload

---
+++ Class2.java
@@ -0,0 +1,9 @@
+public class Class2 {
+    int x = 10;
+  
+    public static void main(String[] args) {
+      Class2 myObj = new Class2();
+      myObj.x = 25; // x is now 25
+      System.out.println(myObj.x);
+    }
+  }
